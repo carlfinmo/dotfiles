a73d607d04a4221b66bfb30ad9368cb69ab0864d
cf <cf@localhost>
2023-03-18 15:17:09 +0100

    add formatting shortcut

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -24,6 +24,7 @@ lsp.on_attach(function(client, bufnr)
 
   vim.keymap.set("n", "<leader>d", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+  vim.keymap.set("n", "<leader>ff", function() vim.lsp.buf.format() end, opts)
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
   vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
   vim.keymap.set("n", "[e", function() vim.diagnostic.goto_next() end, opts)

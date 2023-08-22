d19ffcfd5345461b66c6f747767310ba449bbf48
cf <cf@localhost>
2023-08-22 19:05:14 +0200

    add vim telescope config

---
+++ .config/nvim/after/plugin/telescope.lua
@@ -0,0 +1,5 @@
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>Ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>Fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>Fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>Fh', builtin.help_tags, {})

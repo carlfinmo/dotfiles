62a5d80ea88465982bb8f15fb74a6c0be0266e85
cf <cf@localhost>
2024-01-31 18:46:09 +0100

    nvim trouble setup

--- .config/nvim/after/plugin/trouble.lua
+++ .config/nvim/after/plugin/trouble.lua
@@ -1,4 +1,7 @@
 local trouble = require("trouble");
+trouble.setup({
+    icons = false,
+})
 
 vim.keymap.set("n", "]t", function() trouble.next({skip_groups = true, jump = true}); end, opts)
 vim.keymap.set("n", "[t", function() trouble.previous({skip_groups = true, jump = true}); end, opts)

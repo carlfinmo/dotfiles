9f45616b0d07e423cdcfccef3cce2f34115c19a6
cf <cf@localhost>
2024-05-31 08:55:14 +0200

    nvim harpoon v2 and keymaps

--- .config/nvim/after/plugin/harpoon.lua
+++ .config/nvim/after/plugin/harpoon.lua
@@ -1,8 +1,12 @@
 local harpoon = require("harpoon")
 harpoon:setup()
 
-vim.keymap.set("n", "<leader>;", function() harpoon:list():append() end)
-vim.keymap.set("n", "<C-h>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+vim.keymap.set("n", "<leader>h", function()
+	harpoon:list():add()
+end)
+vim.keymap.set("n", "<C-h>", function()
+	harpoon.ui:toggle_quick_menu(harpoon:list())
+end)
 
 -- vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
 -- vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)

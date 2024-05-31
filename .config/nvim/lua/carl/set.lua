7204aed0fa36f8a4c728fdff7a5c2fce7b59b609
cf <cf@localhost>
2024-05-31 09:10:16 +0200

    nvim set.lua (misc stuff)

--- .config/nvim/lua/carl/set.lua
+++ .config/nvim/lua/carl/set.lua
@@ -1,9 +1,9 @@
 vim.opt.filetype = on
 vim.opt.nu = true
 vim.opt.relativenumber = true
-vim.opt.signcolumn = 'yes:1'
+vim.opt.signcolumn = "yes:1"
 
-vim.opt.mouse = ''
+vim.opt.mouse = ""
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
@@ -23,18 +23,31 @@ vim.opt.termguicolors = true
 
 vim.opt.pumheight = 10
 
-vim.keymap.set('v', '<leader>tc', 'y<cr>:call system("tmux load-buffer -w -", @0)<cr>gv')
-vim.keymap.set('v', '<leader>st', 'y<cr>:call system("tmux load-buffer -w -; tmux paste-buffer -t +", @0)<cr>')
-
-vim.api.nvim_set_hl(0, 'White', { fg = "White", bold = true })
-vim.api.nvim_set_hl(0, 'Red', { fg = "Red", bold = true })
-vim.api.nvim_set_hl(0, 'White2', { fg = "#ffffff", undercurl = true })
-
-vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
+-- vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
 
 vim.api.nvim_create_autocmd({ "BufReadPost" }, {
-    pattern = { "*" },
-    callback = function()
-        vim.api.nvim_exec('silent! normal! g`"zv', false)
-    end,
+	pattern = { "*" },
+	callback = function()
+		vim.api.nvim_exec('silent! normal! g`"zv', false)
+	end,
 })
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+	pattern = "*",
+	callback = function()
+		local ret = vim.api.nvim_get_hl(0, { name = "Normal" })
+		vim.api.nvim_set_hl(0, "Unclutter", { fg = ret.bg })
+	end,
+})
+
+vim.keymap.set("n", "<M-1>", ':call matchadd("One", expand("<cword>"))<CR>')
+vim.keymap.set("n", "<M-2>", ':call matchadd("Two", expand("<cword>"))<CR>')
+vim.keymap.set("n", "<M-3>", ':call matchadd("Three", expand("<cword>"))<CR>')
+
+vim.api.nvim_set_hl(0, "One", { fg = "Red", bold = true })
+vim.api.nvim_set_hl(0, "Two", { fg = "White", bold = true })
+vim.api.nvim_set_hl(0, "Three", { fg = "#ef41fc", bold = false })
+vim.api.nvim_set_hl(0, "White", { fg = "White", bold = true })
+vim.api.nvim_set_hl(0, "Red", { fg = "Red", bold = true })
+vim.api.nvim_set_hl(0, "White2", { fg = "#ffffff", undercurl = true })
+vim.api.nvim_set_hl(0, "Grey", { fg = "#989898" })

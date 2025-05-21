41160c9cf2de1904c317ed992b4c1e6ed67311fb
cf <cf@localhost>
2025-05-21 17:44:22 +0200

    nvim: use blink.cmp

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,17 +1,12 @@
 return {
 	"neovim/nvim-lspconfig",
-	dependencies = { "hrsh7th/cmp-nvim-lsp" },
+	dependencies = { "saghen/blink.cmp" },
 	config = function()
 		vim.opt.signcolumn = "yes"
 
 		-- Add cmp_nvim_lsp capabilities settings to lspconfig
 		-- This should be executed before you configure any language server
 		local lspconfig_defaults = require("lspconfig").util.default_config
-		lspconfig_defaults.capabilities = vim.tbl_deep_extend(
-			"force",
-			lspconfig_defaults.capabilities,
-			require("cmp_nvim_lsp").default_capabilities()
-		)
 
 		vim.api.nvim_create_autocmd("LspAttach", {
 			group = vim.api.nvim_create_augroup("UserLspConfig", {}),

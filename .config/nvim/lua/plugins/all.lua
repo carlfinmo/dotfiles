7cda39c483d28988d394c9e30fd0bcf68b1973e6
cf <cf@localhost>
2026-03-08 16:36:11 +0100

    nvim cleanup plugins

--- .config/nvim/lua/plugins/all.lua
+++ .config/nvim/lua/plugins/all.lua
@@ -3,7 +3,6 @@ return {
 
 	"gerazov/toggle-bool.nvim",
 	-- { "rcarriga/nvim-dap-ui", dependencies = { "mfussenegger/nvim-dap" } },
-	"tpope/vim-commentary",
 	"tpope/vim-fugitive",
 	"tpope/vim-surround",
 	"godlygeek/tabular",
@@ -18,32 +17,6 @@ return {
 	-- 	"nvim-orgmode/orgmode",
 	-- 	config = function(
 	-- 		require("orgmode").setup({}
-	-- 	end,
-	-- },
-	-- {
-	-- 	"akinsho/flutter-tools.nvim",
-	-- 	dependencies = {
-	-- 		"nvim-lua/plenary.nvim",
-	-- 		"stevearc/dressing.nvim", -- optional for vim.ui.select
-	-- 	},
-	-- },
-	-- {
-	-- 	"zbirenbaum/copilot.lua",
-	-- 	cmd = "Copilot",
-	-- 	event = "InsertEnter",
-	-- 	config = function(
-	-- 		require("copilot").setup({
-	-- 			suggestion = { enabled = false },
-	-- 			panel = { enabled = false },
-	-- 		}
-	-- 	end,
-	-- },
-	"onsails/lspkind.nvim",
-	-- {
-	-- 	"zbirenbaum/copilot-cmp",
-	-- 	after = { "copilot.lua" },
-	-- 	config = function(
-	-- 		require("copilot_cmp").setup(
 	-- 	end,
 	-- },
 }

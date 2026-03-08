8f3a2ee9d70656bb55d6fac6a8ae29db4f1bb274
cf <cf@localhost>
2026-03-08 16:40:06 +0100

    nvim modernize treesitter.lua

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,106 +2,15 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	event = { "BufReadPre", "BufNewFile" },
-	dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			-- A list of parser names, or "all" (the four listed parsers should always be installed)
-			ensure_installed = { "c", "lua", "vim" },
-
-			-- Install parsers synchronously (only applied to `ensure_installed`)
-			sync_install = false,
-
-			-- Automatically install missing parsers when entering buffer
-			-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-			auto_install = true,
-
-			-- List of parsers to ignore installing (for "all")
-			ignore_install = { "javascript" },
-
-			---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-			-- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-
-			highlight = {
-				-- `false` will disable the whole extension
-				enable = true,
-
-				-- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-				-- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-				-- the name of the parser)
-				-- list of language that will be disabled
-				disable = { "c", "rust" },
-				-- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-				disable = function(lang, buf)
-					local max_filesize = 100 * 1024 -- 100 KB
-					local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-					if ok and stats and stats.size > max_filesize then
-						return true
-					end
-				end,
-
-				-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-				-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-				-- Using this option may slow down your editor, and you may see some duplicate highlights.
-				-- Instead of true it can also be a list of languages
-				additional_vim_regex_highlighting = false,
-			},
-			textobjects = {
-				move = {
-					enable = true,
-					set_jumps = true, -- whether to set jumps in the jumplist
-					goto_next_start = {
-						["]m"] = "@function.outer",
-						["]]"] = { query = "@class.outer", desc = "Next class start" },
-						--
-						-- You can use regex matching (i.e. lua pattern) and/or pass a list in a "query" key to group multiple queires.
-						["]o"] = "@loop.*",
-						-- ["]o"] = { query = { "@loop.inner", "@loop.outer" } }
-						--
-						-- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
-						-- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
-						["]s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
-						["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
-					},
-					goto_next_end = {
-						["]M"] = "@function.outer",
-						["]["] = "@class.outer",
-					},
-					goto_previous_start = {
-						["[m"] = "@function.outer",
-						["[["] = "@class.outer",
-					},
-					goto_previous_end = {
-						["[M"] = "@function.outer",
-						["[]"] = "@class.outer",
-					},
-					-- Below will go to either the start or the end, whichever is closer.
-					-- Use if you want more granular movements
-					-- Make it even more gradual by adding multiple queries and regex.
-					goto_next = {
-						["]d"] = "@conditional.outer",
-					},
-					goto_previous = {
-						["[d"] = "@conditional.outer",
-					},
-				},
+	branch = "main",
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		branch = "main",
+		config = function()
+			require("nvim-treesitter-textobjects").setup({
 				select = {
-					enable = true,
-
 					-- Automatically jump forward to textobj, similar to targets.vim
 					lookahead = true,
-
-					keymaps = {
-						-- You can use the capture groups defined in textobjects.scm
-						["af"] = "@function.outer",
-						["if"] = "@function.inner",
-						["ac"] = "@class.outer",
-						["ik"] = "@comment.outer",
-						["ak"] = "@comment_block.outer",
-
-						-- You can optionally set descriptions to the mappings (used in the desc parameter of
-						-- nvim_buf_set_keymap) which plugins like which-key display
-						["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-					},
 					-- You can choose the select mode (default is charwise 'v')
 					--
 					-- Can also be a function which gets passed a table with the keys
@@ -125,16 +34,83 @@ return {
 					-- and should return true of false
 					include_surrounding_whitespace = false,
 				},
-				swap = {
-					enable = true,
-					swap_next = {
-						["<leader>a"] = "@parameter.inner",
-					},
-					swap_previous = {
-						["<leader>A"] = "@parameter.inner",
-					},
-				},
+			})
+		end,
+		keys = {
+			{
+				"<leader>a",
+				function()
+					require("nvim-treesitter-textobjects.swap").swap_next("@parameter.inner")
+				end,
+				desc = "Swap next parameter",
+				mode = { "n" },
 			},
-		})
-	end,
+			-- {
+			-- 	"<leader>A",
+			-- 	function()
+			-- 		require("nvim-treesitter-textobjects.swap").swap_previous("@parameter.outer")
+			-- 	end,
+			-- 	desc = "Swap previous parameter",
+			-- 	mode = { "n" },
+			-- },
+			{
+				"af",
+				function()
+					require("nvim-treesitter-textobjects.select").select_textobject("@function.outer", "textobjects")
+				end,
+				desc = "Select outer function",
+				mode = { "x", "o" },
+			},
+			{
+				"if",
+				function()
+					require("nvim-treesitter-textobjects.select").select_textobject("@function.inner", "textobjects")
+				end,
+				desc = "Select inner function",
+				mode = { "x", "o" },
+			},
+			{
+				"ac",
+				function()
+					require("nvim-treesitter-textobjects.select").select_textobject("@class.outer", "textobjects")
+				end,
+				desc = "Select outer class",
+				mode = { "x", "o" },
+			},
+			{
+				"ic",
+				function()
+					require("nvim-treesitter-textobjects.select").select_textobject("@class.inner", "textobjects")
+				end,
+				desc = "Select inner class",
+				mode = { "x", "o" },
+			},
+			{
+				"ik",
+				function()
+					require("nvim-treesitter-textobjects.select").select_textobject("@comment.inner", "textobjects")
+				end,
+				desc = "Select inner comment",
+				mode = { "x", "o" },
+			},
+			{
+				"ok",
+				function()
+					require("nvim-treesitter-textobjects.select").select_textobject("@comment.outer", "textobjects")
+				end,
+				desc = "Select outer comment",
+				mode = { "x", "o" },
+			},
+			{
+				"as",
+				function()
+					require("nvim-treesitter-textobjects.select").select_textobject("@local.scope", "locals")
+				end,
+				desc = "Select local scope",
+				mode = { "x", "o" },
+			},
+		},
+		---@module "nvim-treesitter-textobjects"
+		opts = { multiwindow = true },
+	},
 }

91ef13a08bcb1e880042bf31656f558dd959fd77
cf <cf@localhost>
2026-03-08 16:39:31 +0100

    nvim modernize telescope.lua

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,5 @@
 return {
 	"nvim-telescope/telescope.nvim",
-	tag = "0.1.8",
 	dependencies = {
 		{
 			"nvim-lua/plenary.nvim",
@@ -8,14 +7,28 @@ return {
 		},
 	},
 	config = function()
-		require("telescope").setup()
+		local actions = require("telescope.actions")
+		require("telescope").setup({
+			pickers = {
+				live_grep = {
+					mappings = {
+						i = { ["<c-f>"] = actions.to_fuzzy_refine },
+					},
+				},
+			},
+		})
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>f", builtin.find_files, {})
 		vim.keymap.set("n", "<leader>g", builtin.live_grep, {})
+		vim.keymap.set("n", "<leader>G", builtin.git_status, {})
 		-- vim.keymap.set("n", "<leader>g", "<cmd>Telescope live_grep<cr>", { desc = "Fuzzy find recent files" })
 		vim.keymap.set("n", "<leader>Fb", builtin.buffers, {})
 		vim.keymap.set("n", "<leader>Fh", builtin.help_tags, {})
 		vim.keymap.set("n", "<leader>T", builtin.resume, {})
+		vim.keymap.set("n", "<leader>*", function()
+			local word = vim.fn.expand("<cword>")
+			builtin.live_grep({ default_text = word })
+		end, { noremap = true })
 		-- require("telescope").load_extension("ui-select")
 	end,
 }
